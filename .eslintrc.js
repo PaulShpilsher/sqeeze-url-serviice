acb394906386ad576011eca79fefd408a28bab4f
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-07 12:50:37 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-unused-vars": "warn",
+  },
+};
