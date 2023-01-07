feb43f9899ea4d477f1df5cc62406d66c5d9ef2f
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-07 20:21:35 +0300

    add getLongUrlService

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -1,23 +1,11 @@
 import { UserUrl } from "@prisma/client";
-import { addShortUrl } from "./../repositories/url.repository";
-import { ServiceError } from "..//types/service-error";
+import {
+  addShortUrl,
+  findByShortUrlCode,
+} from "../repositories/url.repository";
 import { generateShortCode } from "./short-code-generator.service";
+import { ServiceError } from "../types/service-error";
 
-export const submitUrlService = async (
-  longUrl: string,
-  shortUrlCode?: string
-) => {
-  // TODO: proper url validation: properly formed and not pointing to another short url
-  if (!/^(http|https):\/\//.test(longUrl)) {
-    throw new ServiceError("Invalid long URL", 400);
-  }
-
-  if (!shortUrlCode) {
-    return await submitGeneratedUrlCode(longUrl);
-  } else {
-    return await submitUserDefineddUrlCode(longUrl, shortUrlCode);
-  }
-};
 
 const submitUserDefineddUrlCode = async (
   longUrl: string,
@@ -48,4 +36,33 @@ const submitGeneratedUrlCode = async (longUrl: string) => {
     }
   }
   throw new ServiceError("Unable to generate unique code", 500);
+};
+
+
+export const submitUrlService = async (
+  longUrl: string,
+  shortUrlCode?: string
+) => {
+  // TODO: proper url validation: properly formed and not pointing to another short url
+  if (!/^(http|https):\/\//.test(longUrl)) {
+    throw new ServiceError("Invalid long URL", 400);
+  }
+
+  if (!shortUrlCode) {
+    return await submitGeneratedUrlCode(longUrl);
+  } else {
+    return await submitUserDefineddUrlCode(longUrl, shortUrlCode);
+  }
+};
+
+export const getLongUrlService = async (shortUrlCode: string) => {
+  if (!shortUrlCode) {
+    throw new ServiceError("Missing short URL code", 400);
+  }
+
+  const userUrl = await findByShortUrlCode(shortUrlCode);
+  if (!userUrl) {
+    throw new ServiceError("Invalid short url code", 404);
+  }
+  return userUrl.longUrl;
 };
