72df1a5afe28e23553e458c3832f7eb1cfc1c5f7
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-07 13:54:01 +0300

    add submit route

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,27 @@ import Koa from "koa";
 import Router from "koa-router";
 import json from "koa-json";
 import logger from "koa-logger";
+import bodyParser from "koa-bodyparser";
+import { SubnitRequest } from "./types/submit.request";
 
 const app = new Koa();
 const router = new Router();
 
 router.get("/health", async (ctx, next) => {
-  ctx.body = {
-    status: "Running",
-  };
-
+  ctx.body = { status: "Running" };
   await next();
 });
 
-app.use(json()).use(logger());
+router.post("/submit", async (ctx, next) => {
+  const { url } = ctx.request.body as SubnitRequest;
+  ctx.body = { msg: 'req', url: url };
+  await next();
+});
+
+app.use(json()).use(logger()).use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT ?? "3000";
-
 app.listen(Number(port), () => {
-    console.log(`Service started on port: ${port}`);
+  console.log(`Service started on port: ${port}`);
 });
---
+++ src/types/submit.request.ts
@@ -0,0 +1,3 @@
+export interface SubnitRequest {
+    url: string;
+}
